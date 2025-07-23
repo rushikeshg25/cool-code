85c527a89aedd54515f475df97328a9cabb0cd42
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-07-23 14:27:57 +0530

    ref: removed spotify clone from root

--- Spotify-clone/prisma/migrations/20250721051349_init_recently_played_songs/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "RecentlyPlayedSong" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "artist" TEXT NOT NULL,
-    "album" TEXT,
-    "playedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
